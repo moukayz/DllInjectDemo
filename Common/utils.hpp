d7c562a2eb4584e19a62001a733fd606a96b120f
mouka <mouka0905@gmail.com>
2018-11-26 01:37:08 +0800

    ime injection (half implemented)

--- Common/utils.hpp
+++ Common/utils.hpp
@@ -5,7 +5,14 @@
 #include <strsafe.h>
 #include "..\Common\log.hpp"
 
-inline VOID ErrorExit( LPCSTR lpszFunction )
+typedef struct _GUI_INFO
+{
+	HWND	hWindow;
+	DWORD	ThreadId;
+	DWORD	ProcessId;
+}GUI_INFO, *PGUI_INFO;
+
+inline VOID ErrorExit(LPCSTR lpszFunction)
 {
 	// Retrieve the system error message for the last-error code
 
@@ -19,117 +26,171 @@ inline VOID ErrorExit( LPCSTR lpszFunction )
 		FORMAT_MESSAGE_IGNORE_INSERTS,
 		NULL,
 		dw,
-		MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		(LPTSTR)&lpMsgBuf,
-		0, NULL );
+		0, NULL);
 
 	// Display the error message and exit the process
 
-	lpDisplayBuf = (LPVOID)LocalAlloc( LMEM_ZEROINIT,
-		( lstrlen( (LPCTSTR)lpMsgBuf ) + lstrlen( (LPCTSTR)lpszFunction ) + 40 ) * sizeof( TCHAR ) );
-	StringCchPrintf( (LPTSTR)lpDisplayBuf,
-		LocalSize( lpDisplayBuf ) / sizeof( TCHAR ),
-		TEXT( "%s ---- error code %d: %s" ),
-		lpszFunction, dw, lpMsgBuf );
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%s ---- error code %d: %s"),
+		lpszFunction, dw, lpMsgBuf);
 	//MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
-	log_error( (LPCSTR)lpDisplayBuf );
+	log_error((LPCSTR)lpDisplayBuf);
 
-	LocalFree( lpMsgBuf );
-	LocalFree( lpDisplayBuf );
-	system( "PAUSE" );
-	ExitProcess( dw );
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+	system("PAUSE");
+	ExitProcess(dw);
 }
 
-inline DWORD FindProcessId( const char *processname )
+inline DWORD FindProcessId(const char *processname)
 {
 	HANDLE hProcessSnap;
 	PROCESSENTRY32 pe32;
 	DWORD result = NULL;
 
 	// Take a snapshot of all processes in the system.
-	hProcessSnap = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 );
-	if ( INVALID_HANDLE_VALUE == hProcessSnap ) return( FALSE );
+	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (INVALID_HANDLE_VALUE == hProcessSnap) return(FALSE);
 
-	pe32.dwSize = sizeof( PROCESSENTRY32 ); // <----- IMPORTANT
+	pe32.dwSize = sizeof(PROCESSENTRY32); // <----- IMPORTANT
 
 											// Retrieve information about the first process,
 											// and exit if unsuccessful
-	if ( !Process32First( hProcessSnap, &pe32 ) )
+	if (!Process32First(hProcessSnap, &pe32))
 	{
-		CloseHandle( hProcessSnap );          // clean the snapshot object
-		ErrorExit( TEXT( " Failed to gather information on system processes! " ) );
+		CloseHandle(hProcessSnap);          // clean the snapshot object
+		ErrorExit(TEXT(" Failed to gather information on system processes! "));
 	}
 
 	do
 	{
-		if ( 0 == strcmp( processname, pe32.szExeFile ) )
+		if (0 == strcmp(processname, pe32.szExeFile))
 		{
 			result = pe32.th32ProcessID;
 			break;
 		}
-	} while ( Process32Next( hProcessSnap, &pe32 ) );
+	} while (Process32Next(hProcessSnap, &pe32));
 
-	CloseHandle( hProcessSnap );
+	CloseHandle(hProcessSnap);
 
 	return result;
 }
 
-inline DWORD GetMainThreadId( DWORD dwOwnerPID )
+inline DWORD GetMainThreadId(DWORD dwOwnerPID)
 {
 	HANDLE  hThreadSnap = NULL;
 	THREADENTRY32 te32;
 
 	// Take a snapshot of all running threads
-	if ( ( hThreadSnap = CreateToolhelp32Snapshot( TH32CS_SNAPTHREAD, 0 ) ) ==
-		INVALID_HANDLE_VALUE )
+	if ((hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0)) ==
+		INVALID_HANDLE_VALUE)
 	{
-		ErrorExit( TEXT( "Cannot create thread snapshot!" ) );
+		ErrorExit(TEXT("Cannot create thread snapshot!"));
 
 	}
 
-	te32.dwSize = sizeof( THREADENTRY32 );
+	te32.dwSize = sizeof(THREADENTRY32);
 
 	// Retrieve infomation about the frist thread
-	if ( !Thread32First( hThreadSnap, &te32 ) )
+	if (!Thread32First(hThreadSnap, &te32))
 	{
-		ErrorExit( TEXT( "Cannot get the handle of the first thread!" ) );
+		ErrorExit(TEXT("Cannot get the handle of the first thread!"));
 
 	}
 
-	while ( te32.th32OwnerProcessID != dwOwnerPID )
+	while (te32.th32OwnerProcessID != dwOwnerPID)
 	{
-		Thread32Next( hThreadSnap, &te32 );
-		if ( te32.th32OwnerProcessID == dwOwnerPID )
+		Thread32Next(hThreadSnap, &te32);
+		if (te32.th32OwnerProcessID == dwOwnerPID)
 			return te32.th32ThreadID;
 	}
 
 	return 0;
 }
 
-inline HANDLE GetMainThread( DWORD dwOwnerPID )
+inline HANDLE GetMainThread(DWORD dwOwnerPID)
 {
 	HANDLE hThread = NULL;
 
 	// Return the main thread's handle
-	if ( !( hThread = OpenThread( THREAD_ALL_ACCESS, FALSE, GetMainThreadId(dwOwnerPID)) ) )
+	if (!(hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, GetMainThreadId(dwOwnerPID))))
 	{
-		ErrorExit( TEXT( "Open main thread of the remote process failed!" ) );
+		ErrorExit(TEXT("Open main thread of the remote process failed!"));
 	}
 
 	return hThread;
 
 }
 
-inline BOOL FileExists( LPCTSTR szPath )
+inline BOOL FileExists(LPCTSTR szPath)
 {
-	DWORD dwAttrib = GetFileAttributes( szPath );
+	DWORD dwAttrib = GetFileAttributes(szPath);
 
-	return ( dwAttrib != INVALID_FILE_ATTRIBUTES &&
-		!( dwAttrib & FILE_ATTRIBUTE_DIRECTORY ) );
+	return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
+		!(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 }
 
-
-inline HANDLE GetProcessWindow( DWORD Pid )
+inline BOOL CALLBACK EnumWindowsProc(
+	HWND   hwnd,
+	LPARAM lParam
+)
 {
-	EnumWindows()
+	DWORD pid, tid = 0;
+	PGUI_INFO guiInfo = (PGUI_INFO)lParam;
+
+	tid = GetWindowThreadProcessId(hwnd, &pid);
+	if (!tid)	ErrorExit("Get window process failed.");
+
+	if (pid == guiInfo->ProcessId)
+	{
+		guiInfo->hWindow = hwnd;
+		guiInfo->ThreadId = tid;
+		SetLastError(ERROR_SUCCESS);
+		return FALSE;
+	}
+
+	return TRUE;
 }
+
+inline BOOLEAN GetProcessGUIThreadInfo(PGUI_INFO pGUIInfo)
+{
+	if (!EnumWindows((WNDENUMPROC)EnumWindowsProc, (LPARAM)pGUIInfo) &&
+		GetLastError() != ERROR_SUCCESS)
+		return FALSE;
+
+	return TRUE;
+		//HANDLE  hThreadSnap = NULL;
+		//THREADENTRY32 te32;
+		//GUITHREADINFO guiInfo = { 0 };
+		//guiInfo.cbSize = sizeof(GUITHREADINFO);
+
+		//// Take a snapshot of all running threads
+		//hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+		//if (hThreadSnap == INVALID_HANDLE_VALUE)
+		//	ErrorExit(TEXT("Cannot create thread snapshot!"));
+
+		//te32.dwSize = sizeof(THREADENTRY32);
+		//// Retrieve infomation about the frist thread
+		//if (!Thread32First(hThreadSnap, &te32))
+		//	ErrorExit(TEXT("Cannot get the handle of the first thread!"));
+
+
+		//while (te32.th32OwnerProcessID != dwOwnerPID)
+		//{
+		//	Thread32Next(hThreadSnap, &te32);
+		//	// Check if the thread belongs to target process and has GUI 
+		//	if (te32.th32OwnerProcessID == dwOwnerPID &&
+		//		GetGUIThreadInfo(te32.th32ThreadID, &guiInfo))
+		//	{
+		//		return te32.th32ThreadID;
+		//	}
+		//}
+
+		//return 0;
+}
+
--- IME_Injector/Source.cpp
+++ IME_Injector/Source.cpp
@@ -1,20 +1,80 @@
 #include <Windows.h>
 #include <tchar.h>
+#include <imm.h>
+#include <ShlObj.h>
+#include <strsafe.h>
 
 #include "..\Common\log.hpp"
 #include "..\Common\utils.hpp"
 
-#define DLLPATH	_T("C:\\Users\\MOUKA\\Source\\Repos\\DLLInjectDemo\\Bin\\MyDll.dll")
-#define IMEPATH	_T("C:\\Users\\MOUKA\\Source\\Repos\\DLLInjectDemo\\Bin\\MyDll.ime")
-#define TARGET_PROCESS	_T("windbg.exe")
+#pragma comment(lib, "imm32.lib")
+
+#define DLLPATH	_T("C:\\UInject.ime")
+#define IMEPATH	_T("R:\\DLLInjectDemo\\Bin\\MyDll.ime")
+#define IME_NAME	_T("MyDll")
+#define TARGET_PROCESS	_T("MyWindowProgram.exe")
 
 int _tmain()
 {
+	CHAR	windir[MAX_PATH] = { 0 };
+	CHAR	imeDir[MAX_PATH] = { 0 };
 	// Check dll is valid
-	if ( !FileExists( DLLPATH ) )	ErrorExit( "Injected dll not exist." );
+	if (!FileExists(DLLPATH))	ErrorExit("Injected dll not exist.");
 
+	SHGetSpecialFolderPathA(0, windir, CSIDL_MYMUSIC, FALSE);
+
+	StringCbPrintfA(imeDir, MAX_PATH, "%s\\%s", windir, "MyDll.ime");
 	// Copy dll to ime file
-	if ( !CopyFile( DLLPATH, IMEPATH, FALSE ) )	ErrorExit( "Copy dll to ime failed." );
+	if (!CopyFile(DLLPATH, imeDir, FALSE))	ErrorExit("Copy dll to ime failed.");
 
+	DWORD pid = FindProcessId(TARGET_PROCESS);
+	if (!pid)	ErrorExit("Get process id failed.");
+
+	GUI_INFO guiInfo = { 0 };
+	guiInfo.ProcessId = pid;
+	if (!GetProcessGUIThreadInfo(&guiInfo))
+		ErrorExit("Get GUI thread failed.");
+
+	HKL hIME = ImmInstallIME(DLLPATH, "UInject");
+	if (!hIME)	ErrorExit("Install ime failed.");
+
+	PostMessage(guiInfo.hWindow, WM_INPUTLANGCHANGEREQUEST, INPUTLANGCHANGE_SYSCHARSET, (LPARAM)hIME);
+	PostMessage(guiInfo.hWindow, WM_INPUTLANGCHANGE, 0, (LPARAM)hIME);
+	Sleep(1000);
+
+	// Remove ime from system after injection
+	if (!UnloadKeyboardLayout(hIME))
+		ErrorExit("Unload inject IME failed.");
+
+	HKEY hKey = 0;
+	DWORD valuesCount = 0;
+	TCHAR valueName[MAX_PATH] = { 0 };
+	DWORD valueNameSize = MAX_PATH;
+
+	if (ERROR_SUCCESS == RegOpenKeyEx(
+		HKEY_CURRENT_USER,
+		TEXT("Keyboard Layout\\Preload"),
+		0,
+		KEY_ALL_ACCESS,
+		&hKey)
+		)
+	{
+		if (ERROR_SUCCESS == RegQueryInfoKey(hKey, NULL, NULL, NULL, NULL, NULL, NULL, &valuesCount, NULL, NULL, NULL, NULL))
+		{
+			if (ERROR_SUCCESS == RegEnumValue(hKey, valuesCount - 1, valueName, &valueNameSize, NULL, NULL, NULL, NULL))
+			{
+				RegDeleteValue(hKey, valueName);
+			}
+			else
+				ErrorExit("Enum value failed.");
+		}
+		else
+			ErrorExit("Query key info failed.");
+
+	}
+	else
+		ErrorExit("Open reg key failed.");
+
+	return 0;
 
 }
