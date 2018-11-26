d0e08e9b349e87f84efd3cbddbf71f61c2dba358
铮 杨 <mouka0905@outlook.com>
2018-11-26 18:57:40 +0800

    fix IME injection incompletely remove relative registry

--- IME_Injector/Source.cpp
+++ IME_Injector/Source.cpp
@@ -11,8 +11,8 @@
 
 /*
 IME Injection:
-ime file can be any dll 
-* "ime" extension not matter, 
+ime file can be any dll
+* "ime" extension not matter,
 * no need to implement any ime function
 
 *** need version resource which specify it's a input method
@@ -22,11 +22,13 @@ FILESUBTYPE 0xbL	(VFT2_DRV_INPUTMETHOD)
 */
 
 #define DLLPATH	_T("C:\\UInject.ime")
-#define DLLPATH2 _T("D:\\repos\\DllInjectDemo\\Bin\\MyDll.dll")
+#define DLLPATH2 _T("C:\\Users\\MOUKA\\Source\\Repos\\DLLInjectDemo\\Bin\\MyDll.dll")
 #define IME_FILENAME	_T("MyDll.dll")
 #define IME_NAME	_T("MyDll")
 #define TARGET_PROCESS	_T("MyWindowProgram.exe")
 
+#define REG_CURRENT_USER_KBDLAYOUT	_T("Keyboard Layout\\Preload")
+#define REG_LOCAL_MACHINE_KBDLAYOUT	_T("SYSTEM\\ControlSet001\\Control\\Keyboard Layouts")
 int _tmain()
 {
 	CHAR	windir[MAX_PATH] = { 0 };
@@ -51,7 +53,6 @@ int _tmain()
 	if ( !GetProcessGUIThreadInfo( &guiInfo ) )
 		ErrorExit( "Get GUI thread failed." );
 
-	system( "PAUSE" );
 	HKL hIME = ImmInstallIME( imeDir, "MyDll" );
 	if ( !hIME )
 	{
@@ -59,11 +60,20 @@ int _tmain()
 		ErrorExit( "Install ime failed. " );
 	}
 
+	// Backup default ime
+	HKL	oldIme = 0;
+	SystemParametersInfo( SPI_GETDEFAULTINPUTLANG, 0, &oldIme, 0 );
+
 	PostMessage( guiInfo.hWindow, WM_INPUTLANGCHANGEREQUEST, INPUTLANGCHANGE_SYSCHARSET, (LPARAM)hIME );
 	PostMessage( guiInfo.hWindow, WM_INPUTLANGCHANGE, 0, (LPARAM)hIME );
 	Sleep( 1000 );
 
-	// Remove ime from system after injection
+	//// Restore default ime in target window
+	//PostMessage( guiInfo.hWindow, WM_INPUTLANGCHANGEREQUEST, INPUTLANGCHANGE_SYSCHARSET, (LPARAM)oldIme );
+	//PostMessage( guiInfo.hWindow, WM_INPUTLANGCHANGE, 0, (LPARAM)oldIme );
+	//Sleep( 1000 );
+
+	// Remove inject ime from system after injection
 	if ( !UnloadKeyboardLayout( hIME ) )
 		ErrorExit( "Unload inject IME failed." );
 
@@ -71,10 +81,12 @@ int _tmain()
 	DWORD valuesCount = 0;
 	TCHAR valueName[MAX_PATH] = { 0 };
 	DWORD valueNameSize = MAX_PATH;
+	TCHAR subKeyName[MAX_PATH] = { 0 };
+
 
 	if ( ERROR_SUCCESS == RegOpenKeyEx(
 		HKEY_CURRENT_USER,
-		TEXT( "Keyboard Layout\\Preload" ),
+		REG_CURRENT_USER_KBDLAYOUT,
 		0,
 		KEY_ALL_ACCESS,
 		&hKey )
@@ -92,9 +104,44 @@ int _tmain()
 		else
 			ErrorExit( "Query key info failed." );
 
+		RegCloseKey( hKey );
 	}
 	else
 		ErrorExit( "Open reg key failed." );
+
+	DWORD ret = ERROR_SUCCESS;
+	DWORD idx = 0;
+	DWORD keyIme = 0;
+
+	if ( ERROR_SUCCESS == RegOpenKeyEx(
+		HKEY_LOCAL_MACHINE,
+		REG_LOCAL_MACHINE_KBDLAYOUT,
+		0, KEY_ALL_ACCESS, &hKey ) )
+	{
+		while ( RegEnumKey( hKey, idx++, subKeyName, MAX_PATH ) == ERROR_SUCCESS )
+		{
+			keyIme = _tcstoul( subKeyName, NULL, 16 );
+			if ( keyIme > 0x100000 )
+				log_debug( "Delete ime key !" );
+			if ( keyIme == (DWORD)hIME )
+			{
+				DebugBreak();
+				if ( ERROR_SUCCESS == RegDeleteKey( hKey, subKeyName ) )
+				{
+					log_debug( "Delete ime key !" );
+					break;
+				}
+				else
+					ErrorExit( "Delete ime key failed." );
+
+			}
+
+		}
+	}
+	else
+		ErrorExit( "Open ime key failed." );
+
+
 
 	system( "PAUSE" );
 	return 0;
