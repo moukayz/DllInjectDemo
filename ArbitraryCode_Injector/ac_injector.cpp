20a6bbd24d84396bef8b0b82ec40ba4662ec3175
铮 杨 <mouka0905@outlook.com>
2018-11-26 12:20:49 +0800

    implement IME injection

--- ArbitraryCode_Injector/ac_injector.cpp
+++ ArbitraryCode_Injector/ac_injector.cpp
@@ -24,40 +24,6 @@ byte wrapper[] = {
 	0xb8,0xff,0xff,0xff,0xff,0xff,0xd0,
 	0xc3 };
 
-DWORD FindProcessId(const char *processname)
-{
-	HANDLE hProcessSnap;
-	PROCESSENTRY32 pe32;
-	DWORD result = NULL;
-
-	// Take a snapshot of all processes in the system.
-	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (INVALID_HANDLE_VALUE == hProcessSnap) return(FALSE);
-
-	pe32.dwSize = sizeof(PROCESSENTRY32); // <----- IMPORTANT
-
-										  // Retrieve information about the first process,
-										  // and exit if unsuccessful
-	if (!Process32First(hProcessSnap, &pe32))
-	{
-		CloseHandle(hProcessSnap);          // clean the snapshot object
-		printf("!!! Failed to gather information on system processes! \n");
-		return(NULL);
-	}
-
-	do
-	{
-		if (0 == strcmp(processname, pe32.szExeFile))
-		{
-			result = pe32.th32ProcessID;
-			break;
-		}
-	} while (Process32Next(hProcessSnap, &pe32));
-
-	CloseHandle(hProcessSnap);
-
-	return result;
-}
 
 int main()
 {
--- IME_Injector/Source.cpp
+++ IME_Injector/Source.cpp
@@ -9,8 +9,21 @@
 
 #pragma comment(lib, "imm32.lib")
 
+/*
+IME Injection:
+ime file can be any dll 
+* "ime" extension not matter, 
+* no need to implement any ime function
+
+*** need version resource which specify it's a input method
+eg:
+FILETYPE 0x3L		(VFT_DRV)
+FILESUBTYPE 0xbL	(VFT2_DRV_INPUTMETHOD)
+*/
+
 #define DLLPATH	_T("C:\\UInject.ime")
-#define IMEPATH	_T("R:\\DLLInjectDemo\\Bin\\MyDll.ime")
+#define DLLPATH2 _T("D:\\repos\\DllInjectDemo\\Bin\\MyDll.dll")
+#define IME_FILENAME	_T("MyDll.dll")
 #define IME_NAME	_T("MyDll")
 #define TARGET_PROCESS	_T("MyWindowProgram.exe")
 
@@ -19,62 +32,71 @@ int _tmain()
 	CHAR	windir[MAX_PATH] = { 0 };
 	CHAR	imeDir[MAX_PATH] = { 0 };
 	// Check dll is valid
-	if (!FileExists(DLLPATH))	ErrorExit("Injected dll not exist.");
+	if ( !FileExists( DLLPATH2 ) )	ErrorExit( "Injected dll not exist." );
 
-	SHGetSpecialFolderPathA(0, windir, CSIDL_MYMUSIC, FALSE);
+	SHGetSpecialFolderPathA( 0, windir, CSIDL_SYSTEM, FALSE );
 
-	StringCbPrintfA(imeDir, MAX_PATH, "%s\\%s", windir, "MyDll.ime");
+	StringCbPrintfA( imeDir, MAX_PATH, "%s\\%s", windir, "MyDll.dll" );
+	log_debug( "Ime path: %s", imeDir );
+
+
 	// Copy dll to ime file
-	if (!CopyFile(DLLPATH, imeDir, FALSE))	ErrorExit("Copy dll to ime failed.");
+	if ( !CopyFile( DLLPATH2, imeDir, FALSE ) )	ErrorExit( "Copy dll to ime failed." );
 
-	DWORD pid = FindProcessId(TARGET_PROCESS);
-	if (!pid)	ErrorExit("Get process id failed.");
+	DWORD pid = FindProcessId( TARGET_PROCESS );
+	if ( !pid )	ErrorExit( "Get process id failed." );
 
 	GUI_INFO guiInfo = { 0 };
 	guiInfo.ProcessId = pid;
-	if (!GetProcessGUIThreadInfo(&guiInfo))
-		ErrorExit("Get GUI thread failed.");
+	if ( !GetProcessGUIThreadInfo( &guiInfo ) )
+		ErrorExit( "Get GUI thread failed." );
 
-	HKL hIME = ImmInstallIME(DLLPATH, "UInject");
-	if (!hIME)	ErrorExit("Install ime failed.");
+	system( "PAUSE" );
+	HKL hIME = ImmInstallIME( imeDir, "MyDll" );
+	if ( !hIME )
+	{
+		log_error( "hIME = %p\n", hIME );
+		ErrorExit( "Install ime failed. " );
+	}
 
-	PostMessage(guiInfo.hWindow, WM_INPUTLANGCHANGEREQUEST, INPUTLANGCHANGE_SYSCHARSET, (LPARAM)hIME);
-	PostMessage(guiInfo.hWindow, WM_INPUTLANGCHANGE, 0, (LPARAM)hIME);
-	Sleep(1000);
+	PostMessage( guiInfo.hWindow, WM_INPUTLANGCHANGEREQUEST, INPUTLANGCHANGE_SYSCHARSET, (LPARAM)hIME );
+	PostMessage( guiInfo.hWindow, WM_INPUTLANGCHANGE, 0, (LPARAM)hIME );
+	Sleep( 1000 );
 
 	// Remove ime from system after injection
-	if (!UnloadKeyboardLayout(hIME))
-		ErrorExit("Unload inject IME failed.");
+	if ( !UnloadKeyboardLayout( hIME ) )
+		ErrorExit( "Unload inject IME failed." );
 
 	HKEY hKey = 0;
 	DWORD valuesCount = 0;
 	TCHAR valueName[MAX_PATH] = { 0 };
 	DWORD valueNameSize = MAX_PATH;
 
-	if (ERROR_SUCCESS == RegOpenKeyEx(
+	if ( ERROR_SUCCESS == RegOpenKeyEx(
 		HKEY_CURRENT_USER,
-		TEXT("Keyboard Layout\\Preload"),
+		TEXT( "Keyboard Layout\\Preload" ),
 		0,
 		KEY_ALL_ACCESS,
-		&hKey)
+		&hKey )
 		)
 	{
-		if (ERROR_SUCCESS == RegQueryInfoKey(hKey, NULL, NULL, NULL, NULL, NULL, NULL, &valuesCount, NULL, NULL, NULL, NULL))
+		if ( ERROR_SUCCESS == RegQueryInfoKey( hKey, NULL, NULL, NULL, NULL, NULL, NULL, &valuesCount, NULL, NULL, NULL, NULL ) )
 		{
-			if (ERROR_SUCCESS == RegEnumValue(hKey, valuesCount - 1, valueName, &valueNameSize, NULL, NULL, NULL, NULL))
+			if ( ERROR_SUCCESS == RegEnumValue( hKey, valuesCount - 1, valueName, &valueNameSize, NULL, NULL, NULL, NULL ) )
 			{
-				RegDeleteValue(hKey, valueName);
+				RegDeleteValue( hKey, valueName );
 			}
 			else
-				ErrorExit("Enum value failed.");
+				ErrorExit( "Enum value failed." );
 		}
 		else
-			ErrorExit("Query key info failed.");
+			ErrorExit( "Query key info failed." );
 
 	}
 	else
-		ErrorExit("Open reg key failed.");
+		ErrorExit( "Open reg key failed." );
 
+	system( "PAUSE" );
 	return 0;
 
 }
